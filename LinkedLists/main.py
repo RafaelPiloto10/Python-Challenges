1803abeaffedab0e3d5ffd96e38e42da71e7bef9
Rafael PilotoVSchool <rpilotojr@icloud.com>
2018-01-11 11:19:05 -0600

    Solved Linked Lists challenge. Answer included in comments

--- LinkedLists/main.py
+++ LinkedLists/main.py
@@ -1,23 +1,30 @@
 #Used to make requests
+# Answer is http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=66831
+
 import urllib.request
 import re
-y = 16044 / 2
+
+url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=' # Python challenges website
+nextNothing = 16044 / 2 # Start at this value for faster answer could also start at 12345
+stringPattern = r'next nothing is \d*' # Re expression for the string 'next nothing  <int>
+numberPattern = r'\d+' # Re expression for just the integer value
+
+def getNextNothing(pattern, string):
+    # Easy function for finding and returning the match
+    return re.search(pattern, string)
 
 while True:
-    #print(i)
-    pasty = y
-    x = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=' + str(y))
-    content = x.read()
-    expre = re.compile(r"next nothing is 63579 \d")
-    nothing = re.findall(expre, str(content))
-    tempString = ""
-    for i in nothing:
-        tempString += i
-    answer = re.findall("\d")
+    # Iterate through the websites
     
-    y =("".join(nothing))
-    print(y)
-##    if x.getcode() == 200 and "and the next" not in x.read():
-##        print(y)
-##        break
+    x = urllib.request.urlopen(url + str(nextNothing)) # Open Html page
+    content = x.read() # Read html page
+
+    nextNothingString = getNextNothing(stringPattern, str(content)) # Get next nothing string
+    string = nextNothingString.group() # turn the group into a string
+
+    nextNothingInt = getNextNothing(numberPattern, string) # Get next nothing int
+    nextNothing = nextNothingInt.group() # Set the next y to be the next nothing
+    print(nextNothing) # Print the nextNothing
+
+
 
