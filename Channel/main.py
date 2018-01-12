e3034539a7034ae400dcddc3bffde2428ca5e4ed
Rafael Piloto <rpilotojr@icloud.com>
2018-01-11 18:34:15 -0600

    Found solution to challenge

--- Channel/main.py
+++ Channel/main.py
@@ -1,8 +1,17 @@
 # Source : 'http://www.pythonchallenge.com/pc/def/channel.html'
-import urllib.request
+import zipfile, re
 
-url = 'http://www.pythonchallenge.com/pc/def/channel.html'
-content = urllib.request.urlopen(url)
+f = zipfile.ZipFile("zipSource/channel.zip")
+start = "90052"
+comments = []
 
-for a, b in zip(' now there are pairs', content):
-    print(a, b)
+while True:
+	content = f.read(start + ".txt").decode("utf-8")
+	comments.append(f.getinfo(start + ".txt").comment.decode("utf-8"))
+	print(content)
+	match = re.search("Next nothing is (\d+)", content)
+	if match == None:
+		break
+	start = match.group(1)
+
+print("".join(comments))
